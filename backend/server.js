36aef4d42d995a0549290a6af64c30bc412b26b3
Utkarsh060903 <utkarsh060903@gmail.com>
2024-07-28 00:29:00 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,3 @@
-// import express from 'express'
-// import mongoose from 'mongoose'
-// import cors from 'cors'
-// import { connectDB } from './config/db.js';
-// import preBookRouter from './Routes/PreBookRoute.js';
-// import retrieveRouter from './Routes/retrieveRouter.js';
-
-// const app = express();
-
-// const port = 4000;
-
-// app.use(express.json())
-
-// app.use(cors())
-
-// app.use('/api' , preBookRouter)
-// app.use('/info' , retrieveRouter)
-
-// connectDB();
-
-// app.listen(port , ()=>{
-//     console.log(`Server started on http://localhost:${port}`)
-// })
-
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
